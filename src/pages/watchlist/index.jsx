f717eb04f369bd1bab2ab90504288a2dc53ea85b
Insha Bano <inshabano88158@gmail.com>
2024-10-30 03:19:54 +0530

    sort-based-on-genre

--- src/pages/watchlist/index.jsx
+++ src/pages/watchlist/index.jsx
@@ -13,11 +13,8 @@ const Watchlist = ({ movies, removeFromWatchlist }) => {
   const [uniqueGenres, setUniqueGenres] = useState([ALL_GENRES]);
   const [selectedGenre, setSelectedGenre] = useState(ALL_GENRES);
 
-
-
   useEffect(() => {
     const genreSet = new Set();
-
     movies.forEach((movie) => {
       movie.genre_ids.forEach((genreId) => {
         const genre = Moviegenres.find((gen) => gen.id === genreId);
@@ -31,7 +28,7 @@ const Watchlist = ({ movies, removeFromWatchlist }) => {
 
   return (
     <>
-      <div className="flex items-center justify-center gap-8 mt-6 mb-10 ">
+      <div className="flex items-center justify-center gap-8 mt-6 mb-10 cursor-pointer">
         {uniqueGenres.map((genre, index) => (
           <div
             key={index}
@@ -45,25 +42,22 @@ const Watchlist = ({ movies, removeFromWatchlist }) => {
           </div>
         ))}
       </div>
-      <div className="flex items-center justify-center m-8 ">
+      <div className="flex items-center justify-center m-8">
         <input
-          className="bg-slate-200 p-3 enabled:hover:bg-slate-300 transition duration-300 transform hover:scale-110 disabled:opacity-75 w-[20rem] text-black outline-none" // changed text color for better visibility
+          className="bg-slate-200 p-3 enabled:hover:bg-slate-300 transition duration-300 transform hover:scale-110 disabled:opacity-75 w-[20rem] text-black outline-none"
           type="text"
           placeholder="Search Movie"
         />
       </div>
       <div className="flex items-center justify-center">
         <table className="rounded-xl border-1 w-[90%] overflow-hidden">
-          <thead className="bg-slate-900 h-12 w-full rounded-lg ">
+          <thead className="bg-slate-900 h-12 w-full rounded-lg">
             <tr className="text-left border-b-[1px]">
               <th className="pl-12">Name</th>
               <th className="flex gap-1 items-center pt-3">
                 <FontAwesomeIcon icon={faArrowUp} className="cursor-pointer" />
                 <span>Ratings</span>
-                <FontAwesomeIcon
-                  icon={faArrowDown}
-                  className="cursor-pointer"
-                />
+                <FontAwesomeIcon icon={faArrowDown} className="cursor-pointer" />
               </th>
               <th>Release Date</th>
               <th>Genre</th>
@@ -72,39 +66,47 @@ const Watchlist = ({ movies, removeFromWatchlist }) => {
           </thead>
           <tbody>
             {Array.isArray(movies) &&
-              movies.map((movie) => {
-                const movieGenres = movie.genre_ids
-                  .map((id) => {
-                    const genre = Moviegenres.find((genre) => genre.id === id);
-                    return genre ? genre.name : "Unknown Genre";
-                  })
-                  .join(", "); // Join genres for display
+              movies
+                .filter((movie) => {
+                  
+                  if (selectedGenre === ALL_GENRES) return true;
 
-                return (
-                  <tr
-                    key={movie.id}
-                    className="border-b-[1px] hover:bg-zinc-100"
-                  >
-                    <td className="flex gap-2 items-center p-4">
-                      <img
-                        className="h-[120px] w-[180px] rounded-lg"
-                        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-                        alt={movie.title}
-                      />
-                      <p>{movie.title}</p>
-                    </td>
-                    <td className="pl-5">{movie.vote_average}</td>
-                    <td>{movie.release_date}</td>
-                    <td>{movieGenres}</td>
-                    <td className="text-rose-500 cursor-pointer">
-                      <FontAwesomeIcon
-                        onClick={() => removeFromWatchlist(movie)}
-                        icon={faTrash}
-                      />
-                    </td>
-                  </tr>
-                );
-              })}
+                  const movieGenres = movie.genre_ids.map((id) => {
+                    const genre = Moviegenres.find((gen) => gen.id === id);
+                    return genre ? genre.name : null;
+                  });
+                  return movieGenres.includes(selectedGenre);
+                })
+                .map((movie) => {
+                  const movieGenres = movie.genre_ids
+                    .map((id) => {
+                      const genre = Moviegenres.find((genre) => genre.id === id);
+                      return genre ? genre.name : "Unknown Genre";
+                    })
+                    .join(", ");
+
+                  return (
+                    <tr key={movie.id} className="border-b-[1px] hover:bg-zinc-100">
+                      <td className="flex gap-2 items-center p-4">
+                        <img
+                          className="h-[120px] w-[180px] rounded-lg"
+                          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                          alt={movie.title}
+                        />
+                        <p>{movie.title}</p>
+                      </td>
+                      <td className="pl-5">{movie.vote_average}</td>
+                      <td>{movie.release_date}</td>
+                      <td>{movieGenres}</td>
+                      <td className="text-rose-500 cursor-pointer">
+                        <FontAwesomeIcon
+                          onClick={() => removeFromWatchlist(movie)}
+                          icon={faTrash}
+                        />
+                      </td>
+                    </tr>
+                  );
+                })}
           </tbody>
         </table>
       </div>
